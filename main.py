0ed4943a1554d63c7159487521787006df85955c
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-09 15:16:03 +0200

    Tworzenie listy, pętla

---
+++ main.py
@@ -0,0 +1,42 @@
+# Day 20 - Tworzenie listy
+
+owoce = ["jabłko", "banan", "wiśnia"]
+print(owoce)
+
+# Indeksowanie elementówlisty
+print(owoce[0]) #wypisze "jabłko"
+print(owoce[1]) #wypisze "banan"
+
+#Zmiana elementów listy
+
+owoce[0] = "gruszka"
+print(owoce) #wypisze ["gruszka", "banan", "wiśnia"]
+
+# Zadanie na dzisiaj:
+
+#     1.Utwórz listę z pięcioma swoimi ulubionymi owocami.
+#     2.Wypisz na ekranie drugi owoc z listy.
+#     3.Zmień trzeci owoc na coś innego.
+#     4.Wypisz na ekranie pierwszy i trzeci owoc z listy.
+
+owoce = ["kiwi", "agrest", "morela", "truskawka", "liczi"]
+print(owoce)
+print(owoce[1])
+owoce[3] = "banan"
+print(owoce[0])
+print(owoce[3])
+
+# Zadanie dodatkowe
+# Stwórz listę przechowującą nazwy Twoich ulubionych napojów.
+# Następnie, przy pomocy komendy if (poznanej niedawno), wyświetl na ekranie "Ta nazwa jest krótka",
+# jeśli nazwa 1 elementu listy jest krótsza od 10 znaków,
+# lub "Ta nazwa jest długa", jeśli jest dłuższa niż 10 znaków.
+napoje = ["sok", "woda", "tonik", "lemoniada"]
+zmienna1 = "Ta nazwa jest krótka"
+zmienna2 = "Ta nazwa jest długa"
+if napoje mniejsze od 10:
+    print(zmienna1)
+    print(len)("napoje")
+if napoje większe od 10:
+    print(zmienna2)
+
